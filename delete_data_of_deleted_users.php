efb4effbdff6b5391626b3b0b97642b4249fdd55
Tobias Zwick <osm@westnordost.de>
2020-04-22 19:15:17 +0200

    switch to another method of putting the deleted users into a temp table
    the previous solution would only work if mysql would run on the same machine as php, but this is not always a given. So now, it inserts them in transactions of 10k chunks and continues where it left of (because it was cancelled)

--- delete_data_of_deleted_users.php
+++ delete_data_of_deleted_users.php
@@ -19,28 +19,41 @@ require_once 'config.php';
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$url = "https://planet.openstreetmap.org/users_deleted/users_deleted.txt";
-$path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "users_deleted.txt";
-// step 1: download file into this directory 
-file_put_contents($path, fopen($url, 'r'));
-
-// step 2: create temporary table to load the data into
+// step 1: create temporary table to load deleted users data into
 $mysqli = new mysqli(Config::DB_HOST, Config::DB_USER, Config::DB_PASS, Config::DB_NAME);
 $mysqli->query(
-    'CREATE TEMPORARY TABLE deleted_user_ids (
+    'CREATE TABLE IF NOT EXISTS deleted_user_ids (
       user_id BIGINT UNSIGNED PRIMARY KEY
     )'
 );
 
-// step 3: load the data directly from file into the database
-// (much faster than programmatic inserts)
-$mysqli->query(
-    "LOAD DATA INFILE '" . addslashes($path) . "' 
-      INTO TABLE deleted_user_ids
-      LINES TERMINATED BY '\n'
-      IGNORE 1 LINES;"
-);
-// step 4: delete the rows belonging to deleted users from the other tables
+$row = $mysqli->query('SELECT user_id FROM deleted_user_ids ORDER BY user_id DESC LIMIT 1;')->fetch_row();
+$highest_deleted_user_id = $row ? $row[0] : 0;
+
+// step 2: download file, parse and insert
+$url = "https://planet.openstreetmap.org/users_deleted/users_deleted.txt";
+$handle = fopen($url, 'r');
+$mysqli->begin_transaction();
+$count = 0;
+while (($line = fgets($handle)) !== false) {
+    $user_id = intval($line);
+    if(!$user_id || $user_id <= $highest_deleted_user_id) continue;
+
+    $stmt = $mysqli->prepare('INSERT INTO deleted_user_ids (user_id) VALUES (?)');
+    $stmt->bind_param('i', $user_id);
+    $stmt->execute();
+    $stmt->close();
+    $count++;
+
+    if ($count % 10000 == 0) {
+        $mysqli->commit();
+        //echo "".$user_id."\n";
+        $mysqli->begin_transaction();
+    }
+}
+$mysqli->commit();
+
+// step 3: delete the rows belonging to deleted users from the other tables
 $mysqli->query(
     'DELETE FROM changesets_walker_state
       WHERE user_id IN (SELECT user_id FROM deleted_user_ids)'
